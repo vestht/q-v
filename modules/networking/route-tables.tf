712cedd828be4e887dc03b86b40fda224364c398
Shyamin Ayesh <me@shyamin.com>
2024-05-18 18:51:01 +0530

    fix indentation

--- modules/networking/route-tables.tf
+++ modules/networking/route-tables.tf
@@ -1,15 +1,22 @@
+# Resource: private_route_table
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id
+	vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name = "VESTHT-VPC-PRIVATE-RTB"
-  }
+	tags = {
+		Name = "VESTHT-VPC-PRIVATE-RTB"
+	}
 }
 
+# Resource: public_route_table
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
+	vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name = "VESTHT-VPC-PUBLIC-RTB"
-  }
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_internet_gateway.internet_gateway.id
+	}
+
+	tags = {
+		Name = "VESTHT-VPC-PUBLIC-RTB"
+	}
 }
