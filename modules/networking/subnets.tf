1acc3d4c5418f5eed1811dd75b29a37e76e31596
Shyamin Ayesh <me@shyamin.com>
2024-05-18 18:37:40 +0530

    initial subnets draft

--- modules/networking/subnets.tf
+++ modules/networking/subnets.tf
@@ -1,3 +1,4 @@
+# Resource: private_subnet_1
 resource "aws_subnet" "private_subnet_1" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "10.0.10.0/24"
@@ -7,11 +8,32 @@ resource "aws_subnet" "private_subnet_1" {
     }
 }
 
+# Resource: private_subnet_2
 resource "aws_subnet" "private_subnet_2" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "10.0.11.0/24"
 
     tags = {
         Name = "VESTHT-VPC-PRIVATE-SUBNET-2"
+    }
+}
+
+# Resource: public_subnet_1
+resource "aws_subnet" "public_subnet_1" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = "10.0.20.0/24"
+
+    tags = {
+        Name = "VESTHT-VPC-PUBLIC-SUBNET-1"
+    }
+}
+
+# Resource: public_subnet_2
+resource "aws_subnet" "public_subnet_2" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = "10.0.21.0/24"
+
+    tags = {
+        Name = "VESTHT-VPC-PUBLIC-SUBNET-2"
     }
 }
